ae850fcd60b99ba026946832acc9d69886dd9c50
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2022-12-05 20:34:04 -0300

    :page_facing_up: signin and signup created

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 import theme from "./styles/theme";
-import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
