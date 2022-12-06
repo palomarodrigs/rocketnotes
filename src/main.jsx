0e88bb9a5b585d54eae7abd2999d468fe7d0b2f4
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2022-12-06 09:04:06 -0300

    :pencil2: create notes

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import theme from "./styles/theme";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Profile } from "./pages/Profile";
+import { New } from "./pages/New";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 );
